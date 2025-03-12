670a9fcd7a40d61f279fc3c1205635c35704f766
Setthawut Jan-arporn <setthj96@wintl.postbox.in.th>
2025-03-12 13:55:08 +0700

    Add Routetable_Subnet-01

---
+++ 0-Implement/04-route-table.tf
@@ -0,0 +1,11 @@
+resource "huaweicloud_vpc_route_table" "custom_route_subnet-01" {
+  name        = "custom_route_subnet-01"
+  vpc_id      = huaweicloud_vpc.example.id
+  subnets =    huaweicloud_vpc_subnet.subnet001.ids
+
+#   route {
+#     destination = "172.16.0.0/16"
+#     type        = "peering"
+#     nexthop     = var.vpc_peering_id
+#   }
+}
