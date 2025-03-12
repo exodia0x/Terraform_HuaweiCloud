452e257034b946446444c1c79a75a106f69c3fc9
Setthawut Jan-arporn <setthj96@wintl.postbox.in.th>
2025-03-12 13:47:01 +0700

    Update

---
+++ 0-Implement/02-subnet.tf
@@ -0,0 +1,17 @@
+// Subnet 001 //
+resource "huaweicloud_vpc_subnet" "subnet001" {
+  name              = "subnet-01"
+  cidr              = "192.168.98.0/28"
+  gateway_ip        = "192.168.98.1"
+  vpc_id            = huaweicloud_vpc.example.id
+  availability_zone = "ap-southeast-3a"
+}
+
+// Subnet 002 //
+resource "huaweicloud_vpc_subnet" "subnet002" {
+  name              = "subnet-02"
+  cidr              = "192.168.98.16/28"
+  gateway_ip        = "192.168.98.17"
+  vpc_id            = huaweicloud_vpc.example.id
+  availability_zone = "ap-southeast-3b"
+}
---
+++ 0-Implement/03-acls.tf
@@ -0,0 +1,54 @@
+resource "huaweicloud_vpc_network_acl" "subnet-01" {
+  name                  = "acls-subnet-01"
+  description           = "acls-subnet-01"
+  enterprise_project_id = 0
+  enabled               = true
+
+  # ingress_rules {
+  #   action                 = "deny"
+  #   ip_version             = 4
+  #   protocol               = "all"
+  #   source_ip_address      = "0.0.0.0/0"
+  #   destination_ip_address = "0.0.0.0/0"
+  # }
+
+  egress_rules {
+    action                 = "allow"
+    ip_version             = 4
+    protocol               = "any"
+    source_ip_address      = "0.0.0.0/0"
+    destination_ip_address = "0.0.0.0/0"
+  }
+
+  associated_subnets {
+    subnet_id = huaweicloud_vpc_subnet.subnet001.id
+  }
+}
+
+
+resource "huaweicloud_vpc_network_acl" "subnet-012" {
+  name                  = "acls-subnet-02"
+  description           = "acls-subnet-02"
+  enterprise_project_id = 0
+  enabled               = true
+
+  # ingress_rules {
+  #   action                 = "deny"
+  #   ip_version             = 4
+  #   protocol               = "all"
+  #   source_ip_address      = "0.0.0.0/0"
+  #   destination_ip_address = "0.0.0.0/0"
+  # }
+
+  egress_rules {
+    action                 = "allow"
+    ip_version             = 4
+    protocol               = "any"
+    source_ip_address      = "0.0.0.0/0"
+    destination_ip_address = "0.0.0.0/0"
+  }
+
+  associated_subnets {
+    subnet_id = huaweicloud_vpc_subnet.subnet002.id
+  }
+}
