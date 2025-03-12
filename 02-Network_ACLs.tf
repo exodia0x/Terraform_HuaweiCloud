001beeff961a0af5ae670e2b7a0650b0bb757f6e
Setthawut Jan-arporn <setthj96@wintl.postbox.in.th>
2025-03-12 10:02:18 +0700

    Update Acls

--- 02-Network_ACLs.tf
+++ 02-Network_ACLs.tf
@@ -4,23 +4,24 @@ resource "huaweicloud_vpc_network_acl" "subnet-01" {
   enterprise_project_id = 0
   enabled               = true
 
-  ingress_rules {
-    action                 = "deny"
-    ip_version             = 4
-    protocol               = "all"
-    source_ip_address      = "0.0.0.0/0"
-    destination_ip_address = "0.0.0.0/0"
-  }
+  # ingress_rules {
+  #   action                 = "deny"
+  #   ip_version             = 4
+  #   protocol               = "all"
+  #   source_ip_address      = "0.0.0.0/0"
+  #   destination_ip_address = "0.0.0.0/0"
+  # }
 
-  egress_rules {
-    action                 = "allow"
-    ip_version             = 4
-    protocol               = "all"
-    source_ip_address      = "0.0.0.0/0"
-    source_port            = "all"
-    destination_ip_address = "0.0.0.0/0"
-    destination_port       = "all"
-  }
+  # egress_rules {
+  #   action                 = "allow"
+  #   ip_version             = 4
+  #   protocol               = "all"
+  #   source_ip_address      = "0.0.0.0/0"
+  #   source_port            = "all"
+  #   destination_ip_address = "0.0.0.0/0"
+  #   destination_port       = "all"
+  # }
+
   associated_subnets {
     subnet_id = huaweicloud_vpc_subnet.subnet001.id
   }
@@ -33,23 +34,24 @@ resource "huaweicloud_vpc_network_acl" "subnet-012" {
   enterprise_project_id = 0
   enabled               = true
 
-  ingress_rules {
-    action                 = "deny"
-    ip_version             = 4
-    protocol               = "all"
-    source_ip_address      = "0.0.0.0/0"
-    destination_ip_address = "0.0.0.0/0"
-  }
+  # ingress_rules {
+  #   action                 = "deny"
+  #   ip_version             = 4
+  #   protocol               = "all"
+  #   source_ip_address      = "0.0.0.0/0"
+  #   destination_ip_address = "0.0.0.0/0"
+  # }
 
-  egress_rules {
-    action                 = "allow"
-    ip_version             = 4
-    protocol               = "all"
-    source_ip_address      = "0.0.0.0/0"
-    source_port            = "all"
-    destination_ip_address = "0.0.0.0/0"
-    destination_port       = "all"
-  }
+  # egress_rules {
+  #   action                 = "allow"
+  #   ip_version             = 4
+  #   protocol               = "all"
+  #   source_ip_address      = "0.0.0.0/0"
+  #   source_port            = "all"
+  #   destination_ip_address = "0.0.0.0/0"
+  #   destination_port       = "all"
+  # }
+
   associated_subnets {
     subnet_id = huaweicloud_vpc_subnet.subnet002.id
   }
