ac43337a90e0a73ec8fa3222167f14f2d9e57c28
Setthawut Jan-arporn <setthj96@wintl.postbox.in.th>
2025-03-12 13:58:13 +0700

    Add Routetable_Subnet-01

--- 0-Implement/04-route-table.tf
+++ 0-Implement/04-route-table.tf
@@ -1,7 +1,7 @@
 resource "huaweicloud_vpc_route_table" "custom_route_subnet-01" {
   name        = "custom_route_subnet-01"
   vpc_id      = huaweicloud_vpc.example.id
-  subnets =    [huaweicloud_vpc_subnet.subnet001.id]
+  # subnets =    [huaweicloud_vpc_subnet.subnet001.id]
 
 #   route {
 #     destination = "172.16.0.0/16"
