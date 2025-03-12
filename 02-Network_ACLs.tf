57607c0e4c5c554a33dbea62a36bdf660d16d013
Setthawut Jan-arporn <setthj96@wintl.postbox.in.th>
2025-03-12 09:57:33 +0700

    Update Acls

--- 02-Network_ACLs.tf
+++ 02-Network_ACLs.tf
@@ -5,42 +5,52 @@ resource "huaweicloud_vpc_network_acl" "subnet-01" {
   enabled               = true
 
   ingress_rules {
+    action                 = "deny"
+    ip_version             = 4
+    protocol               = "all"
+    source_ip_address      = "0.0.0.0/0"
+    destination_ip_address = "0.0.0.0/0"
+  }
+
+  egress_rules {
     action                 = "allow"
     ip_version             = 4
-    protocol               = "tcp"
-    source_ip_address      = "192.168.0.0/24"
-    source_port            = "22-30,33"
+    protocol               = "all"
+    source_ip_address      = "0.0.0.0/0"
+    source_port            = "all"
     destination_ip_address = "0.0.0.0/0"
-    destination_port       = "8001-8010"
+    destination_port       = "all"
   }
+  associated_subnets {
+    subnet_id = huaweicloud_vpc_subnet.subnet001.id
+  }
+}
+
+
+resource "huaweicloud_vpc_network_acl" "subnet-012" {
+  name                  = "acls-subnet-02"
+  description           = "acls-subnet-02"
+  enterprise_project_id = 0
+  enabled               = true
 
   ingress_rules {
     action                 = "deny"
     ip_version             = 4
-    protocol               = "icmp"
-    source_ip_address      = "192.168.0.0/24"
+    protocol               = "all"
+    source_ip_address      = "0.0.0.0/0"
     destination_ip_address = "0.0.0.0/0"
   }
 
   egress_rules {
     action                 = "allow"
     ip_version             = 4
-    protocol               = "tcp"
-    source_ip_address      = "172.16.0.0/24"
-    source_port            = "22-30,33"
+    protocol               = "all"
+    source_ip_address      = "0.0.0.0/0"
+    source_port            = "all"
     destination_ip_address = "0.0.0.0/0"
-    destination_port       = "8001-8010"
+    destination_port       = "all"
   }
-
-  egress_rules {
-    action                 = "deny"
-    ip_version             = 4
-    protocol               = "icmp"
-    source_ip_address      = "172.16.0.0/24"
-    destination_ip_address = "0.0.0.0/0"
-  }
-
   associated_subnets {
-    subnet_id = huaweicloud_vpc_subnet.subnet001.id
+    subnet_id = huaweicloud_vpc_subnet.subnet002.id
   }
 }
