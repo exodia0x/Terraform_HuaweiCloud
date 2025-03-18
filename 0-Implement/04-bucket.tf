5f75f3d396f41d8ffc34e9f3da4993e11d4cf64b
Setthawut Jan-arporn <setthj96@wintl.postbox.in.th>
2025-03-18 20:36:07 +0700

    bucket

--- 0-Implement/04-bucket.tf
+++ 0-Implement/04-bucket.tf
@@ -1,5 +1,5 @@
 resource "huaweicloud_obs_bucket" "bucket-01" {
-  bucket        = "my-tf-test-bucket"
+  bucket        = "my-nanami-test-bucket-01"
   acl           = "private"
   versioning    = true
   storage_class = "STANDARD"
