9b40985812d1404a550e6af45ece61e3e0eb2a7c
Setthawut Jan-arporn <setthj96@wintl.postbox.in.th>
2025-03-18 20:29:50 +0700

    update 001

--- Wait/04-vpc-peering.tf
+++
@@ -1,5 +0,0 @@
-resource "huaweicloud_vpc_peering_connection" "peering" {
-  name        = "Peering-VPC-Ex1-Ex2"
-  vpc_id      = huaweicloud_vpc.example.id
-  peer_vpc_id = huaweicloud_vpc.example2.id
-}
