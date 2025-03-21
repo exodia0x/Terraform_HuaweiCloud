6f6f245b3703617968866dd0f76e27dd5d01c873
Setthawut Jan-arporn <setthj96@wintl.postbox.in.th>
2025-03-21 15:29:34 +0700

    as-config2

--- 0-Implement/03-AS-Configuration.tf
+++ 0-Implement/03-AS-Configuration.tf
@@ -2,13 +2,14 @@ resource "huaweicloud_as_configuration" "my_as_config" {
   scaling_configuration_name = "my_as_config"
 
   instance_config {
-    flavor     = data.huaweicloud_compute_flavors.myflavor.ids[0]
-    image      = data.huaweicloud_images_image.myimage.id
-    user_data = <<EOT
+    flavor      = data.huaweicloud_compute_flavors.myflavor.ids[0]
+    image       = data.huaweicloud_images_image.myimage.id
+    instance_id = huaweicloud_compute_instance.basic.id
+    user_data   = <<EOT
         #! /bin/bash
         echo 'root:Nanami@201' | chpasswd -e
         EOT
-    
+
     disk {
       size        = 40
       volume_type = "SSD"
