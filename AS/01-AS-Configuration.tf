4dd1037fecd1f9f803d64171edb076bebc2e2b8b
Setthawut Jan-arporn <setthj96@wintl.postbox.in.th>
2025-03-21 13:18:26 +0700

    Add AS-Config

--- AS/01-AS-Configuration.tf
+++ AS/01-AS-Configuration.tf
@@ -1,19 +1,13 @@
-variable "flavor_id" {}
-variable "image_id" {}
-variable "ssh_key" {}
-variable "security_group_id" {}
-
 resource "huaweicloud_as_configuration" "my_as_config" {
   scaling_configuration_name = "my_as_config"
 
   instance_config {
-    flavor             = var.flavor_id
-    image              = var.image_id
-    key_name           = var.ssh_key
-    security_group_ids = [var.security_group_id]
-
+    flavor     = data.huaweicloud_compute_flavors.myflavor.ids[0]
+    image      = data.huaweicloud_images_image.myimage.id
+    admin_pass = "Nanami@201"
+    
     disk {
-      size        = 40
+      size        = 20
       volume_type = "SSD"
       disk_type   = "SYS"
     }
