2cbe92cd1d957a5ee483acd9b65b4b0676cf93b3
Setthawut Jan-arporn <setthj96@wintl.postbox.in.th>
2025-03-21 14:07:02 +0700

    rm ec2

---
+++ 0-Implement/03-AS-Configuration.tf
@@ -0,0 +1,15 @@
+resource "huaweicloud_as_configuration" "my_as_config" {
+  scaling_configuration_name = "my_as_config"
+
+  instance_config {
+    flavor     = data.huaweicloud_compute_flavors.myflavor.ids[0]
+    image      = data.huaweicloud_images_image.myimage.id
+    admin_pass = "Nanami@201"
+    
+    disk {
+      size        = 20
+      volume_type = "SSD"
+      disk_type   = "SYS"
+    }
+  }
+}
