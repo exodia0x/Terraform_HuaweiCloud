898bd910a7c191faf708a18e0c1aa4154d4d4326
Setthawut Jan-arporn <setthj96@wintl.postbox.in.th>
2025-03-21 15:46:12 +0700

    as-group

--- 0-Implement/04-AS-Group.tf
+++ 0-Implement/04-AS-Group.tf
@@ -1,12 +1,13 @@
 resource "huaweicloud_as_group" "my_as_group" {
   scaling_group_name       = "my_as_group"
   scaling_configuration_id = huaweicloud_as_configuration.my_as_config.id
-  desire_instance_number   = 1
+  desire_instance_number   = 0
   min_instance_number      = 0
   max_instance_number      = 2
   vpc_id                   = huaweicloud_vpc.example.id
   delete_publicip          = true
   delete_instances         = "yes"
+  availability_zones = data.huaweicloud_availability_zones.myaz
 
   networks {
     id = huaweicloud_vpc_subnet.subnet001.id
