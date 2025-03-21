bb3139d2c6019337bf2a7ff884445409dcb017ca
Setthawut Jan-arporn <setthj96@wintl.postbox.in.th>
2025-03-21 16:15:47 +0700

    Add AS-Policy-Time2

--- 0-Implement/05-AS-Policy.tf
+++ 0-Implement/05-AS-Policy.tf
@@ -1,4 +1,4 @@
-### Periodic ###
+### Periodic - ADD ###
 resource "huaweicloud_as_policy" "my_aspolicy_periodic" {
   scaling_policy_name = "my_aspolicy_periodic"
   scaling_policy_type = "RECURRENCE"
@@ -10,6 +10,24 @@ resource "huaweicloud_as_policy" "my_aspolicy_periodic" {
   }
   scheduled_policy {
     launch_time     = "09:15"
+    recurrence_type = "Daily"
+    # start_time      = "2022-11-30T12:00Z"
+    end_time        = "2025-03-30T12:00Z"
+  }
+}
+
+### Periodic - REMOVE ###
+resource "huaweicloud_as_policy" "my_aspolicy_periodic_remove" {
+  scaling_policy_name = "my_aspolicy_periodic_remove"
+  scaling_policy_type = "REMOVE"
+  scaling_group_id    = huaweicloud_as_group.my_as_group.id
+
+  scaling_policy_action {
+    operation       = "REMOVE"
+    instance_number = 1
+  }
+  scheduled_policy {
+    launch_time     = "09:20"
     recurrence_type = "Daily"
     # start_time      = "2022-11-30T12:00Z"
     end_time        = "2025-03-30T12:00Z"
