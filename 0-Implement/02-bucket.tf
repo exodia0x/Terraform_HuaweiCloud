099ae7a7a8e2c107098fde4b7d4413e5038a66ee
Setthawut Jan-arporn <setthj96@wintl.postbox.in.th>
2025-03-24 09:37:50 +0700

    bucket

--- 0-Implement/02-bucket.tf
+++ 0-Implement/02-bucket.tf
@@ -1,5 +1,5 @@
 resource "huaweicloud_obs_bucket" "bucket-01" {
-  bucket        = "my-tf-test-bucket"
+  bucket        = "my-peace-01-tf-test-bucket"
   acl           = "private"
   versioning    = true
   storage_class = "STANDARD"
